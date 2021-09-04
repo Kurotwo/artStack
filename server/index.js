1cf532ffaab3af2928ad54e6b385f2c07eac8702
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2021-09-04 11:02:40 +0800

    Initial code for the whiteboard

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+function onConnection(socket){
+  console.log("Socket connected!")
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
