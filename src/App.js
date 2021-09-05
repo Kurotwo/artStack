04e9a170be399fbfed53082c07db66ff7fca77b1
Carlos Shi <carlos_shi@dlsu.edu.ph>
2021-09-05 22:24:34 +0800

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Landing from './components/Landing.js'
+import Login from './components/Login.js'
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      {/* <Login /> */}
     </div>
   );
 }
